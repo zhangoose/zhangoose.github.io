4895b1cf007fc4f20c98af04a11d5bfaaed97ffa
zhangoose <az177@eden.rutgers.edu>
2014-01-25 22:55:25 -0500

    mouseover event listeners for the face

--- face.js
+++ face.js
@@ -8,7 +8,16 @@ $(".icon").mouseout(function(){
 	$(".icon").attr("src","img/1.png");
 });
 
+$("#title").mouseover(function(){
+	console.log("Yaaaaaaaaaaaaaaaaaaaaaaay");
+	$(".icon").attr("src","img/2.png");
+});
+
+$("#title").mouseout(function(){
+	$(".icon").attr("src","img/1.png");
+});
+
 $(document).on("mousemove",function(event){
-	console.log("X: " + event.pageX);
-	onsole.log("Y: " + event.pageY);
+	//console.log("X: " + event.pageX);
+	//console.log("Y: " + event.pageY);
 });
